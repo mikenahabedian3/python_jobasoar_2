c8e735ec53bf4e43eb5a397289efcad6fd6ff6dc
mikenahabedian3 <mikenahabedian3@gmail.com>
2023-09-17 11:26:35 -0400

    iniitial commit - basic

---
+++ jobasoar3/urls.py
@@ -0,0 +1,14 @@
+# jobasoar3/jobasoar3/urls.py
+
+from django.contrib import admin
+from django.urls import path
+
+from core import views
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    
+    path('', views.home, name='home'),
+
+]
