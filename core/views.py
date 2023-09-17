07fc19ebb6d2f35868fd5c34e3313eff1034548a
mikenahabedian3 <mikenahabedian3@gmail.com>
2023-09-17 14:45:48 -0400

    authentication all working well

--- core/views.py
+++ core/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+# core/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')  # Redirect to dashboard instead of home
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 def home(request):
     return render(request, 'home.html')
+
+@login_required
+def dashboard(request):
+    return render(request, 'core/dashboard.html')
+
+
--- jobasoar3/urls.py
+++ jobasoar3/urls.py
@@ -4,11 +4,10 @@ from django.contrib import admin
 from django.urls import path, include
 from core import views
 
-
 urlpatterns = [
-
     path('admin/', admin.site.urls),
-    
+    path('', include('django.contrib.auth.urls')),
     path('', views.home, name='home'),
-
+    path('signup/', views.signup, name='signup'),  # Add this line for the signup view
+    path('dashboard/', views.dashboard, name='dashboard'),  # Updated to use views.dashboard
 ]
